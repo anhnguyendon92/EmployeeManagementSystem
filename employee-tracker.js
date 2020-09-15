de093b7d42da34aded1593d36c2d2ea247bd1c7e
Anh Nguyen <anhnguyendon92@gmail.com>
2020-09-15 11:13:06 -0700

    updating

--- employee-tracker.js
+++ employee-tracker.js
@@ -25,76 +25,76 @@ connection.connect((err) => {
 });
 
 //Main menu function 
-function mainMenu(){
+function mainMenu() {
     // Prompts
     inquirer
-    .prompt({
-        name: "action",
-        type: "list",
-        message: "Main Menu",
-        choices: [
-            "View all employee",
-            "View all employees by role",
-            "View all employees by department",
-            "View all employees by manager",
-            "Add Employee",
-            "Add Role",
-            "Add department",
-            "Update employee role",
-            "Update employee manager",
-            "Delete employee",
-            "Delete role",
-            "Delete department",
-            "View department budgets"
-        ]
-    })
-    .then((answer) => {
-        // Switch case depending on user option
-        switch (answer.action) {
-            case "View all employees":
-                viewAllEmp();
-                break;
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "Main Menu",
+            choices: [
+                "View all employee",
+                "View all employees by role",
+                "View all employees by department",
+                "View all employees by manager",
+                "Add Employee",
+                "Add Role",
+                "Add department",
+                "Update employee role",
+                "Update employee manager",
+                "Delete employee",
+                "Delete role",
+                "Delete department",
+                "View department budgets"
+            ]
+        })
+        .then((answer) => {
+            // Switch case depending on user option
+            switch (answer.action) {
+                case "View all employees":
+                    viewAllEmp();
+                    break;
 
-            case "View all employees by department":
-                viewAllEmpByDept();
-                break;
+                case "View all employees by department":
+                    viewAllEmpByDept();
+                    break;
 
-            case "View all employees by role":
-                viewAllEmpByRole();
-                break;
+                case "View all employees by role":
+                    viewAllEmpByRole();
+                    break;
 
-            case "Add employee":
-                addEmp();
-                break;
+                case "Add employee":
+                    addEmp();
+                    break;
 
-            case "Add department":
-                addDept();
-                break;
-            case "Add role":
-                addRole();
-                break;
-            case "Update employee role":
-                updateEmpRole();
-                break;
-            case "Update employee manager":
-                updateEmpMngr();
-                break;
-            case "View all employees by manager":
-                viewAllEmpByMngr();
-                break;
-            case "Delete employee":
-                deleteEmp();
-                break;
-            case "View department budgets":
-                viewDeptBudget();
-                break;
-            case "Delete role":
-                deleteRole();
-                break;
-            case "Delete department":
-                deleteDept();
-                break;
-        }
-    });
-    
+                case "Add department":
+                    addDept();
+                    break;
+                case "Add role":
+                    addRole();
+                    break;
+                case "Update employee role":
+                    updateEmpRole();
+                    break;
+                case "Update employee manager":
+                    updateEmpMngr();
+                    break;
+                case "View all employees by manager":
+                    viewAllEmpByMngr();
+                    break;
+                case "Delete employee":
+                    deleteEmp();
+                    break;
+                case "View department budgets":
+                    viewDeptBudget();
+                    break;
+                case "Delete role":
+                    deleteRole();
+                    break;
+                case "Delete department":
+                    deleteDept();
+                    break;
+            }
+        });
+
 }
